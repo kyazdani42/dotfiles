801062a5ee111f2bab9c194e35c646ee16b1935b
kiyan <kyazdani@student.42.fr>
2018-12-09 00:30:10 +0100

    edit install

--- vim/install.sh
+++ vim/install.sh
@@ -1,11 +1,10 @@
-cp -r .vim ${HOME}/.vim
-
 # plugins
 mkdir -p ${HOME}/.vim/autoload ${HOME}/.vim/bundle && \
 git clone https://github.com/scrooloose/nerdtree.git ${HOME}/.vim/bundle/nerdtree && \
 git clone https://github.com/fatih/vim-go.git ${HOME}/.vim/bundle/vim-go && \
 curl -LSso ${HOME}/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+cp -r .vim/* ${HOME}/.vim
 
 # vimrc
 ln -s ${HOME}/.vim/vimrc ${HOME}/.vimrc
