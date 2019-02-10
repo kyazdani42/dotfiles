7452330172ec9efec5dfd6a8e459903d3ba25536
kiyan <yazdani.kiyan@gmail.com>
2019-02-10 20:03:24 +0100

    fix symlink

--- symlinks_dotfiles.sh
+++ symlinks_dotfiles.sh
@@ -1,20 +1,20 @@
 # link home config file
-ln -s .gitconfig $HOME/.gitconfig
-ln -s .profile $HOME/.profile
-ln -s .xinitrc $HOME/.xinitrc
-ln -s .Xresources $HOME/.Xresources
-ln -s .zshrc $HOME/.zshrc
+ln -s $PWD/.gitconfig $HOME/.gitconfig
+ln -s $PWD/.profile $HOME/.profile
+ln -s $PWD/.xinitrc $HOME/.xinitrc
+ln -s $PWD/.Xresources $HOME/.Xresources
+ln -s $PWD/.zshrc $HOME/.zshrc
 
 # link home scripts
-ln -s .scripts $HOME/.scripts
+ln -s $PWD/.scripts $HOME/.scripts
 
 # link vim config
-ln -s .vim $HOME/.vim
+ln -s $PWD/.vim $HOME/.vim
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
 # link .config folders
-ln -s .config/alacritty $HOME/.config/alacritty
-ln -s .config/compton.conf $HOME/.config/compton.conf
-ln -s .config/i3 $HOME/.config/i3
-ln -s .config/i3blocks $HOME/.config/i3blocks
+ln -s $PWD/.config/alacritty $HOME/.config/alacritty
+ln -s $PWD/.config/compton.conf $HOME/.config/compton.conf
+ln -s $PWD/.config/i3 $HOME/.config/i3
+ln -s $PWD/.config/i3blocks $HOME/.config/i3blocks
 
