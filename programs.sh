a0b95f26a86ff898f04e8247d6ad11b7748d17e2
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-05-28 17:44:11 +0200

    make installing programs better

--- programs.sh
+++ programs.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-echo installing programs
+function installer() {
+	which $1 &>/dev/null
+	[ $? == 1 ] && echo "installing $1" && sudo pacman -Sy $1
+}
 
-sudo pacman -S \
-	git \
-	vim \
-	neovim \
-	alacritty \
-	bat \
-	compton \
-	exa \
-	feh \
-	polybar \
-	tmux \
-	vifm \
-	zsh \
-	zathura \
-	nodejs \
-	npm \
-	yarn \
-	net-tools \
-	chromium \
-	firefox \
-	ripgrep
+installer git
+installer vim
+installer alacritty
+installer bat
+installer compton
+installer exa
+installer feh
+installer polybar
+installer tmux
+installer vifm
+installer zsh
+installer zathura
+installer npm
+installer yarn
+installer chromium
+installer firefox
+
+installer nodejs
+installer net-tools
+installer ripgrep
+installer neovim
 
 exit $?
+
