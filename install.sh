9e9bdd891aa0f567b9bb6cea05ab0c09e4e5cfca
kiyan <yazdani.kiyan@protonmail.com>
2019-07-28 17:09:02 +0200

    add more stuff

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-if [ "$1" == "-h" -o "$1" == "--help" ]; then
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 	echo "usage: ./install.sh (--init-system will try to install programs with pacman)"
 	exit 0;
 fi
 
+[ "$1" == "--init" ] || [ "$1" == "-i" ] && exec ./programs.sh;
+
 function handle_errors {
 	if [ $? != 0 ]; then
-		echo $1
+		echo "$1"
 		exit 1
 	fi
 }
 
-[ "$1" == "--init" -o "$1" == "-i" ] && exec ./programs.sh;
-
-if [ ! -d $HOME/.config/base16-shell ]; then
+if [ ! -d "$HOME/.config/base16-shell" ]; then
 	echo -----------------------------------------------------------------------------
 	echo install base16-shell colors
 
-	git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell &>/dev/null
+	git clone https://github.com/chriskempson/base16-shell.git "$HOME/.config/base16-shell" &>/dev/null
 	handle_errors "Error cloning base16-shell colors"
 	echo "base16-shell colors installed"
 fi
 
-if [ ! -d $HOME/.oh-my-zsh ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo -----------------------------------------------------------------------------
 	echo installing oh my zsh
 
@@ -31,27 +31,27 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
 	handle_errors "Error installing oh my zsh"
 
 	#enable vi key binding
-	sed -i 's/bindkey -e/bindkey -v\nexport KEYTIMEOUT=1/g' $HOME/.oh-my-zsh/lib/key-bindings.zsh
+	sed -i 's/bindkey -e/bindkey -v\nexport KEYTIMEOUT=1/g' "$HOME/.oh-my-zsh/lib/key-bindings.zsh"
 	echo "oh my zsh installed"
 
 	echo "installing zsh-autosuggestions"
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &>/dev/null
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" &>/dev/null
 	handle_errors "Error installing zsh-autosuggestions"
 	echo "zsh-autosuggestions installed"
 fi
 
-if [ ! -f $HOME/.zsh_functions/async ]; then
+if [ ! -f "$HOME/.zsh_functions/async" ]; then
 	echo -----------------------------------------------------------------------------
 	echo "installing pure prompt"
-	rm -rf $HOME/.zsh_functions /tmp/pure
+	rm -rf "$HOME/.zsh_functions" /tmp/pure
 
 	git clone https://github.com/sindresorhus/pure /tmp/pure &>/dev/null
 	handle_errors "Error cloning pure prompt"
 
-	mkdir -p $HOME/.zsh_functions
-	mv /tmp/pure/async.zsh /tmp/pure/pure.zsh $HOME/.zsh_functions
-	ln -s $HOME/.zsh_functions/pure.zsh $HOME/.zsh_functions/prompt_pure_setup
-	ln -s $HOME/.zsh_functions/async.zsh $HOME/.zsh_functions/async
+	mkdir -p "$HOME/.zsh_functions"
+	mv /tmp/pure/async.zsh /tmp/pure/pure.zsh "$HOME/.zsh_functions"
+	ln -s "$HOME/.zsh_functions/pure.zsh" "$HOME/.zsh_functions/prompt_pure_setup"
+	ln -s "$HOME/.zsh_functions/async.zsh" "$HOME/.zsh_functions/async"
 	rm -rf /tmp/pure
 	echo "pure prompt installed"
 fi
@@ -59,19 +59,19 @@ fi
 echo -----------------------------------------------------------------------------
 echo link zsh config
 
-rm $HOME/.zshrc
-ln -s $PWD/config/zshrc $HOME/.zshrc
+rm "$HOME/.zshrc"
+ln -s "$PWD/config/zshrc" "$HOME/.zshrc"
 
 echo link vim config
 
-rm -rf $HOME/{.vim,.vimrc}
+rm -rf "$HOME/{.vim,.vimrc}"
 
-ln -s $PWD/config/vim $HOME/.vim
-ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s "$PWD/config/vim" "$HOME/.vim"
+ln -s "$HOME/.vim/vimrc" "$HOME/.vimrc"
 
 
-if [ ! -f $PWD/config/vim/autoload/plug.vim ]; then
-	mkdir -p $PWD/config/vim/autoload
+if [ ! -f "$PWD/config/vim/autoload/plug.vim" ]; then
+	mkdir -p "$PWD/config/vim/autoload"
 	echo install vim plug
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	handle_errors "Error installing vim plug"
@@ -80,21 +80,21 @@ fi
 
 echo link configurations
 
-rm -rf $HOME/{.profile,.xinitrc,.Xresources,Pictures,.gtkrc-2.0}
-rm -rf $HOME/.config/{nvim,alacritty,compton.conf,i3,i3blocks,polybar,tmux,gtk-3.0}
+rm -rf "$HOME/{.profile,.xinitrc,.Xresources,Pictures,.gtkrc-2.0}"
+rm -rf "$HOME/.config/{nvim,alacritty,compton.conf,i3,i3blocks,polybar,tmux,gtk-3.0}"
 
-ln -s $PWD/Pictures $HOME/Pictures
+ln -s "$PWD/Pictures" "$HOME/Pictures"
 
-ln -s $PWD/bin $HOME/.bin
+ln -s "$PWD/bin" "$HOME/.bin"
 
-ln -s $PWD/config/profile $HOME/.profile
-ln -s $PWD/config/xinitrc $HOME/.xinitrc
-ln -s $PWD/config/Xresources $HOME/.Xresources
-ln -s $PWD/config/tmux $HOME/.config/tmux
-ln -s $PWD/config/nvim $HOME/.config/nvim
-ln -s $PWD/config/alacritty $HOME/.config/alacritty
-ln -s $PWD/config/compton.conf $HOME/.config/compton.conf
-ln -s $PWD/config/i3 $HOME/.config/i3
-ln -s $PWD/config/polybar $HOME/.config/polybar
-ln -s $PWD/config/gtk-3.0 $HOME/.config/gtk-3.0
-ln -s $PWD/config/gtkrc-2.0 $HOME/.gtkrc-2.0
+ln -s "$PWD/config/profile" "$HOME/.profile"
+ln -s "$PWD/config/xinitrc" "$HOME/.xinitrc"
+ln -s "$PWD/config/Xresources" "$HOME/.Xresources"
+ln -s "$PWD/config/tmux" "$HOME/.config/tmux"
+ln -s "$PWD/config/nvim" "$HOME/.config/nvim"
+ln -s "$PWD/config/alacritty" "$HOME/.config/alacritty"
+ln -s "$PWD/config/compton.conf" "$HOME/.config/compton.conf"
+ln -s "$PWD/config/i3" "$HOME/.config/i3"
+ln -s "$PWD/config/polybar" "$HOME/.config/polybar"
+ln -s "$PWD/config/gtk-3.0" "$HOME/.config/gtk-3.0"
+ln -s "$PWD/config/gtkrc-2.0" "$HOME/.gtkrc-2.0"
--- programs.sh
+++ programs.sh
@@ -2,28 +2,29 @@
 
 installer="err"
 
-which pacman &>/dev/null
-[ $? == 0 ] && installer="pacman"
+function checkinstaller() {
+	command -v "$1" &>/dev/null
+}
 
-which apt-get &>/dev/null
-[ $? == 0 ] && installer="apt-get install"
+[ "$(checkinstaller "pacman")" == 0 ] && installer="pacman"
 
-which apt &>/dev/null
-[ $? == 0 ] && installer="apt install"
+[ "$(checkinstaller "apt-get")" == 0 ] && installer="apt-get install"
 
-[ installer == "err" ] && echo "Error: no supported package manager found" && exit 1;
+[ "$(checkinstaller "apt")" == 0 ] && installer="apt install"
+
+[ "$installer" == "err" ] && echo "Error: no supported package manager found" && exit 1;
 
 echo "Installer with $installer: continue ?: (y/n)"
-read should_continue
-[ $should_continue != "y" ] && exit 1;
+read -r should_continue
+[ "$should_continue" != "y" ] && exit 1;
 
 function installer() {
 	program=$1
-	[ ! -z "$2" ] && program=$2
-	which $program &>/dev/null
+	[ -n "$2" ] && program=$2
+	command -v "$program" &>/dev/null
 	if [ $? == 1 ]; then
 		echo "installing $1"
-		sudo pacman -Sy $1
+		sudo pacman -Sy "$1"
 		[ $? == 1 ] && echo "$1 was not installed" >> install-log.error
 	fi
 }
