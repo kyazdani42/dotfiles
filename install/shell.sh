40cd21c35ea63140ee5ac4db07077ada6ea61227
kiyan <yazdani.kiyan@protonmail.com>
2019-09-07 12:15:29 +0200

    cleanup shell scripts

--- install/shell.sh
+++ install/shell.sh
@@ -3,21 +3,28 @@
 if ! command -v zsh >/dev/null
 then
 	echo "** installing zsh **"
-	sudo pacman -Sy --noconfirm zsh >/dev/null
-	[ "$?" != "0" ] && echo "Error installing zsh, exiting." && exit 1;
+
+	if ! sudo pacman -Sy --noconfirm zsh >/dev/null
+	then
+		echo "Error installing zsh, exiting."
+		exit 1
+	fi
+
 	echo "-- zsh installed --"
 	echo
 fi
 
 [ "$SHELL" != "/usr/bin/zsh" ] && chsh -s /usr/bin/zsh
 
-
 if [ ! -d "$HOME/.config/base16-shell" ]
 then
 	echo "** installing base16-shell colors **"
 
-	git clone https://github.com/chriskempson/base16-shell.git "$HOME/.config/base16-shell" &>/dev/null
-	[ "$?" != "0" ] && echo "Error installing base16-shell colors, exiting." && exit 1;
+	if ! git clone https://github.com/chriskempson/base16-shell.git "$HOME/.config/base16-shell" &>/dev/null
+	then
+		echo "Error installing base16-shell colors, exiting."
+		exit 1
+	fi
 
 	echo "-- base16-shell colors installed --"
 	echo
@@ -27,9 +34,11 @@ if [ ! -d "$HOME/.oh-my-zsh" ]
 then
 	echo "** installing oh my zsh **"
 
-	git clone https://github.com/robbyrussell/oh-my-zsh "$HOME/.oh-my-zsh" &>/dev/null
-
-	[ "$?" != "0" ] && echo "Error installing oh my zsh, exiting." && exit 1;
+	if ! git clone https://github.com/robbyrussell/oh-my-zsh "$HOME/.oh-my-zsh" &>/dev/null
+	then
+		echo "Error installing oh my zsh, exiting."
+		exit 1
+	fi
 
 	# enable vi keybindings
 	sed -i 's/bindkey -e/bindkey -v\nexport KEYTIMEOUT=1/g' "$HOME/.oh-my-zsh/lib/key-bindings.zsh"
@@ -40,8 +49,13 @@ fi
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]
 then
 	echo "** installing zsh-autosuggestions **"
-	git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" &>/dev/null
-	[ "$?" != "0" ] && echo "Error installing zsh-autosuggestions, exiting." && exit 1;
+
+	if ! git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" &>/dev/null
+	then
+		echo "Error installing zsh-autosuggestions, exiting."
+		exit 1
+	fi
+
 	echo "-- zsh-autosuggestions installed --"
 	echo
 fi
@@ -50,15 +64,17 @@ if [ ! -d "$HOME/.zsh-syntax-highlighting" ]
 then
 	echo "** installing zsh-syntax hilighting **"
 
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" &>/dev/null
+	if ! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" &>/dev/null
+	then
+		echo "Error installing zsh-syntax-highlighting, exiting."
+		exit 1
+	fi
 
-	[ "$?" != "0" ] && echo "Error installing zsh-syntax-highlighting, exiting." && exit 1;
 	echo "-- zsh-syntax-highlighting installed --"
 	echo
 fi
 
 echo "link zsh configuration file"
-
 rm -f "$HOME/.zshrc"
 ln -s "$PWD/config/zshrc" "$HOME/.zshrc"
 
--- install/vim.sh
+++ install/vim.sh
@@ -2,35 +2,48 @@
 
 if ! command -v vim >/dev/null; then
 	echo "** installing vim **"
-	sudo pacman -Sy --noconfirm vim >/dev/null
-	[ "$?" != "0" ] && printf "Error during vim installation, exiting.\n" && exit 1;
+
+	if ! sudo pacman -Sy --noconfirm vim >/dev/null
+	then
+		echo "Error during vim installation, exiting."
+		exit 1
+	fi
+
 	echo "-- vim installed --"
+	echo
 fi
 
 if ! command -v nvim >/dev/null; then
 	echo "** installing neovim **"
-	sudo pacman -Sy --noconfirm neovim >/dev/null
-	[ "$?" != "0" ] && printf "Error during neovim installation, exiting.\n" && exit 1;
+
+	if ! sudo pacman -Sy --noconfirm neovim >/dev/null
+	then
+		echo "Error during neovim installation, exiting."
+		exit 1
+	fi
+
 	echo "-- neovim installed --"
+	echo
 fi
 
-printf "remove vim config file and folder\n"
-rm -f $HOME/{.vim,.vimrc}
-rm -f $HOME/.config/nvim
+echo "remove vim config file and folder"
+rm -f "$HOME"/{.vim,.vimrc}
+rm -f "$HOME/.config/nvim"
 
-printf "install vim config file and folder\n"
+echo "install vim config file and folder"
 ln -s "$PWD/config/vim" "$HOME/.vim"
 ln -s "$HOME/.vim/vimrc" "$HOME/.vimrc"
 
 # because on a freshly new installed system there is no config folder yet
-mkdir -p $HOME/.config
+mkdir -p "$HOME/.config"
 ln -s "$PWD/config/nvim" "$HOME/.config/nvim"
 
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
 	mkdir -p "$HOME/.vim/autoload"
 
-	printf "installing vim plugin manager\n"
+	echo "installing vim plugin manager"
 	curl -sfLo "$HOME/.vim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" >/dev/null
 	echo "vim plug installed"
+	echo
 fi
 
