4e892f56ccec758eec7d0998632efbe68d1eccd0
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-09-07 16:51:58 +0200

    polishing zshrc

--- install/rust.sh
+++ install/rust.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if ! command -v cargo
+if ! command -v cargo >/dev/null
 then
-	echo "installing rust and the toolchain"
+	echo "** installing rust and the toolchain **"
 	curl https://sh.rustup.rs -sSf | sh
 fi
 
