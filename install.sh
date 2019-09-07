8524381f54b49661136577329194a3824f9e99d3
kiyan <yazdani.kiyan@protonmail.com>
2019-09-07 12:56:42 +0200

    cleanup main install, remove compton config and coc-settings.json

--- install.sh
+++ install.sh
@@ -2,24 +2,20 @@
 
 case "$1" in
 	"-h" | "--help")
-		echo "usage: ./install.sh (--init will try to install programs with your package manager)";
-		exit 0;
+		echo "usage: ./install.sh [-i|--init]"
+		exit 0
 		;;
 	"-i" | "--init")
-		./install/programs.sh;
-		;;
-	"*")
+		./install/programs.sh
+		./install/vim.sh
+		./install/rust.sh
+		./install/shell.sh
 		;;
 esac
 
-./install/rust.sh
-./install/shell.sh
-./install/vim.sh
-
-printf "linking home configuration files\n"
-
-rm -r "$HOME/{.profile,.xinitrc,.Xresources,Pictures,.gtkrc-2.0,.bin}"
-
+echo "linking home configuration files"
+echo
+rm -rf "$HOME"/{.profile,.xinitrc,.Xresources,Pictures,.gtkrc-2.0,.bin}
 ln -s "$PWD/config/profile" "$HOME/.profile"
 ln -s "$PWD/config/xinitrc" "$HOME/.xinitrc"
 ln -s "$PWD/config/Xresources" "$HOME/.Xresources"
@@ -27,14 +23,13 @@ ln -s "$PWD/config/gtkrc-2.0" "$HOME/.gtkrc-2.0"
 ln -s "$PWD/Pictures" "$HOME/Pictures"
 ln -s "$PWD/bin" "$HOME/.bin"
 
-printf "link XDG_CONFIG_HOME configuration files\n"
-
-rm -rf "$HOME/.config/{nvim,alacritty,compton.conf,i3,i3blocks,polybar,tmux,gtk-3.0}"
-
+echo "link XDG_CONFIG_HOME configuration files"
+rm -rf "$HOME/.config"/{tmux,alacritty,i3,polybar,gtk-3.0}
 ln -s "$PWD/config/tmux" "$HOME/.config/tmux"
 ln -s "$PWD/config/alacritty" "$HOME/.config/alacritty"
-ln -s "$PWD/config/compton.conf" "$HOME/.config/compton.conf"
 ln -s "$PWD/config/i3" "$HOME/.config/i3"
 ln -s "$PWD/config/polybar" "$HOME/.config/polybar"
 ln -s "$PWD/config/gtk-3.0" "$HOME/.config/gtk-3.0"
+
+echo "installation is done, you may now use your new system !"
 
