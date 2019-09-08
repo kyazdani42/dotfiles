399d04e227bb694fa9a69c71d4709570dfd9794c
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-09-08 15:56:10 +0200

    update shell install

--- install/shell.sh
+++ install/shell.sh
@@ -2,27 +2,40 @@
 
 [ "$SHELL" != "/usr/bin/zsh" ] && chsh -s /usr/bin/zsh
 
-if [ ! -d "$HOME/.config/base16-shell" ]
-then
-	echo "** installing base16-shell colors **"
-
-	if ! git clone https://github.com/chriskempson/base16-shell.git "$HOME/.config/base16-shell" &>/dev/null
-	then
-		echo "Error installing base16-shell colors, exiting."
-		exit 1
-	fi
-
-	echo "-- base16-shell colors installed --"
-	echo
-fi
-
-echo "link zsh configuration file"
+echo "Removing .zshrc, .zsh-syntax-highlighting, .oh-my-zsh"
 rm -f "$HOME"/{.zshrc,.zsh-syntax-highlighting,.oh-my-zsh}
-ln -s "$PWD/config/zsh/zshrc" "$HOME/.zshrc"
-ln -s "$PWD/config/zsh/zsh-syntax-highlighting" "$HOME/.zsh-syntax-highlighting"
-ln -s "$PWD/config/zsh/oh-my-zsh" "$HOME/.oh-my-zsh"
-ln -s "$PWD/config/zsh/zsh-autosuggestions" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+echo
 
-# enable vi key bindings
+echo "removing zsh-autosuggestions"
+rm -f "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+rm -f "$PWD/config/zsh/oh-my-zsh/custom/plugins/zsh-autosuggestions"
+echo
+
+echo "removing base16-shell colors"
+rm -f "$HOME/.config/base16-shell"
+echo
+
+echo "creating symlink for .zshrc"
+ln -s "$PWD/config/zsh/zshrc" "$HOME/.zshrc"
+echo
+
+echo "creating symlink for .zsh-syntax-highlighting"
+ln -s "$PWD/config/zsh/zsh-syntax-highlighting" "$HOME/.zsh-syntax-highlighting"
+echo
+
+echo "creating symlink for .oh-my-zsh"
+ln -s "$PWD/config/zsh/oh-my-zsh" "$HOME/.oh-my-zsh"
+echo
+
+echo "changing zsh emacs key bindings to vi key bindings"
 sed -i 's/bindkey -e/bindkey -v\nexport KEYTIMEOUT=1/g' "$HOME/.oh-my-zsh/lib/key-bindings.zsh"
+echo
+
+echo "creating symlink for plugin zsh-autosuggestions"
+ln -s "$PWD/config/zsh/zsh-autosuggestions" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+echo
+
+echo "creating symlink for base16-shell colors"
+ln -s "$PWD/config/base16-shell" "$HOME/.config/base16-shell"
+echo
 
