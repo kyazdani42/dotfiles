dccd30c20303fd90727616f028329ac5f248a118
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-02-24 22:14:53 +0100

    Window manager changes
    - i3 tabbed layout with mod+t, adjust colors for window titles
    - alt+s toggles polybar display
    - polybar config completely revisited: now its 3 little blocks on
    the top left of the screen

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,10 +7,18 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -q --reload date &
+        MONITOR=$m polybar -q --reload xwindow &
+        MONITOR=$m polybar -q --reload i3 &
+        MONITOR=$m polybar -q --reload volume_and_controls &
+        MONITOR=$m polybar -q --reload mpd &
+    done
 else
-  polybar --reload main &
+    polybar -q --reload date &
+    polybar -q --reload xwindow &
+    polybar -q --reload i3 &
+    polybar -q --reload volume_and_controls &
+    polybar -q --reload mpd &
 fi
 
