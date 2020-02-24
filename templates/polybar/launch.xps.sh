299e4de472c2a4b464a576dad43b88b7d2795298
kiyan42 <yazdani.kiyan@protonmail.com>
2020-02-24 23:37:54 +0100

    start making templates for different computers

---
+++ templates/polybar/launch.xps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -q --reload date &
+        MONITOR=$m polybar -q --reload xwindow &
+        MONITOR=$m polybar -q --reload volume_and_controls &
+    done
+else
+    polybar -q --reload date &
+    polybar -q --reload xwindow &
+    polybar -q --reload volume_and_controls &
+fi
+
