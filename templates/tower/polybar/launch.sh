13ed155684aad5ae88bc0270dc861f1526f4e100
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-02-26 13:50:41 +0100

    better picom conf for rofi

--- templates/tower/polybar/launch.sh
+++ templates/tower/polybar/launch.sh
@@ -10,15 +10,13 @@ if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar -q --reload date &
         MONITOR=$m polybar -q --reload xwindow &
-        MONITOR=$m polybar -q --reload i3 &
-        MONITOR=$m polybar -q --reload volume_and_controls &
-        MONITOR=$m polybar -q --reload mpd &
+        MONITOR=$m polybar -q --reload i3_mpc &
+        MONITOR=$m polybar -q --reload mpd_song &
     done
 else
     polybar -q --reload date &
     polybar -q --reload xwindow &
-    polybar -q --reload i3 &
-    polybar -q --reload volume_and_controls &
-    polybar -q --reload mpd &
+    polybar -q --reload i3_mpc &
+    polybar -q --reload mpd_song &
 fi
 
