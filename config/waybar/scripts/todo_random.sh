afb803cfd80ab5e6441d54582c4c1fd05627ffa7
kiyan42 <yazdani.kiyan@protonmail.com>
2020-05-29 02:13:27 +0200

    do not show 'low' todos in waybar

--- config/waybar/scripts/todo_random.sh
+++ config/waybar/scripts/todo_random.sh
@@ -3,5 +3,9 @@
 tods=`cat $XDG_DATA_HOME/todo.md`
 readarray -t todos <<<"$tods"
 
-idx=$(( $RANDOM % ${#todos[@]} ))
-echo ${todos[$idx]} | tr -d '\-*' | sed 's/LOW/low:/' | sed 's/MED/medium: /' | sed 's/HI/high: /'
+while [ -z "$todo" ] || echo $todo | grep 'LOW' &>/dev/null; do
+    idx=$(( $RANDOM % ${#todos[@]} ))
+    todo=${todos[$idx]}
+done
+
+echo $todo | tr -d '\-*' | sed 's/MED/medium: /' | sed 's/HI/high: /'
--- zprofile
+++ zprofile
@@ -23,6 +23,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
+export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 export LESSHISTFILE="-"
 export HISTFILE="$XDG_CACHE_HOME/bash_history"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
