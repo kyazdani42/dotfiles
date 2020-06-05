e3e5509b6db554ab28ad40116b504aa5311d5c68
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-06-05 10:09:11 +0200

    fix some scripts and change mail synchronizer to anthill

--- config/waybar/scripts/wifi.sh
+++ config/waybar/scripts/wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-res="$(nmcli device status | grep wlp2s0 | grep ' connected' | awk '{print $4}')"
+res="$(nmcli device status | grep wlp | grep ' connected' | awk '{print $4}')"
 
 if [ "$res" != "" ]; then
     printf "connected to \`$res\`"
