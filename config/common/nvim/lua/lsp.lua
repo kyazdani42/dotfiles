a2f9ea135506cb2fdce0172fe8274dcb9b25f529
kiyan42 <yazdani.kiyan@protonmail.com>
2020-09-06 11:49:55 +0200

    add format for rust

--- config/common/nvim/lua/lsp.lua
+++ config/common/nvim/lua/lsp.lua
@@ -20,10 +20,26 @@ local opts = {
   signcolumn = 'yes',
 }
 
+local function fmt(bufnr, cmd)
+  bufnr = bufnr or api.nvim_get_current_buf()
+  local current_file = api.nvim_buf_get_name(bufnr)
+  local stdout = vim.fn.system(cmd..current_file)
+  local as_lines = vim.fn.split(stdout, '\n')
+  table.remove(as_lines, 1)
+  table.remove(as_lines, 1)
+  local cursor = api.nvim_win_get_cursor(0)
+  api.nvim_buf_set_lines(bufnr, 0, -1, false, as_lines)
+  api.nvim_exec('w','')
+  api.nvim_win_set_cursor(0, cursor)
+end
+
 function M.format()
-  local ft = api.nvim_buf_get_option(api.nvim_get_current_buf(), 'ft')
+  local bufnr = api.nvim_get_current_buf()
+  local ft = api.nvim_buf_get_option(bufnr, 'ft')
   if ft == 'javascript' or ft == 'javascriptreact' then
     api.nvim_exec('CocCommand eslint.executeAutofix', '')
+  elseif ft == 'rust' then
+    fmt(bufnr, 'rustfmt --emit stdout ')
   else
     api.nvim_call_function("CocAction", {'format'})
   end
