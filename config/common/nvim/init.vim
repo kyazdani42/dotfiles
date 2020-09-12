cef91b4358252792256a26bd26abe0ede119d5df
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-09-12 14:07:23 +0200

    add tsHlGroup virt text

--- config/common/nvim/init.vim
+++ config/common/nvim/init.vim
@@ -24,6 +24,9 @@ set expandtab                  " expand tab into space by default
 set clipboard^=unnamedplus     " Use system clipboard
 set termguicolors
 set shortmess+=c
+set foldmethod=expr
+set foldexpr=nvim_treesitter#foldexpr()
+set foldlevelstart=99
 
 " Retrieve last position in a file: https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
 au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
--- config/common/nvim/lua/ts.lua
+++ config/common/nvim/lua/ts.lua
@@ -1,6 +1,5 @@
 local api = vim.api
 local M = {}
-local ts_utils = require 'nvim-treesitter.ts_utils'
 
 function M.setup()
   require'nvim-treesitter.configs'.setup {
@@ -22,15 +21,18 @@ function M.setup()
 
   api.nvim_set_keymap('n', 'R', ':write | edit | TSBufEnable highlight<CR>', {});
   api.nvim_exec([[
-    command! ToggleTsVtx lua require'ts'.toggle_ts_virt()
+    command! ToggleTsVtx lua require'ts'.toggle_ts_virt_text()
     hi TsVirtText guifg=#89ddff
     augroup TSVirtualText
       au!
       au BufEnter,CursorMoved,CursorMovedI,WinEnter,CompleteDone,InsertEnter,InsertLeave * lua require'ts'.ts_virt_text()
     augroup END
 
-    nnoremap <C-c> :lua require'ts'._hl_under_cursor()<CR>
-
+    command! ToggleTsHlGroups lua require'ts'.toggle_ts_hl_groups()
+    augroup TSVirtualTextHlGroups
+      au!
+      au BufEnter,CursorMoved,CursorMovedI,WinEnter,CompleteDone,InsertEnter,InsertLeave * lua require'ts'.ts_hl_groups()
+    augroup END
   ]], '')
 end
 
@@ -59,19 +61,40 @@ function M.ts_virt_text()
   api.nvim_buf_set_virtual_text(0, ns_id, cursor[1]-1, {{ node_info, "TsVirtText" }}, {})
 end
 
-function M._hl_under_cursor()
-  local ns = api.nvim_get_namespaces().treesitter_hl
-  local _, lnum, col = unpack(vim.fn.getcurpos())
-  local extmarks = api.nvim_buf_get_extmarks(0, ns, {lnum-1, col-1}, {lnum-1, -1}, { details = true })
-  local ext = extmarks[1]
-  local infos = ext[4]
-  -- TODO: find a way to get the extmark under cursor, for now leave it as is
-  if not infos then return print("[no hl group found]") end
+local hl_virt_enable = false
+local hl_ns_id = api.nvim_create_namespace('TSVirtualTextHlGroups')
 
-  local start = " ["..(ext[2]+1).."|"..ext[3].."]"
-  local end_ = "["..(infos.end_row+1).."|".. infos.end_col.."]"
-  local str = infos.hl_group..start.." -> "..end_
-  print(str)
+function M.toggle_ts_hl_groups()
+  if hl_virt_enable then
+    hl_virt_enable = false
+    api.nvim_buf_clear_namespace(0, hl_ns_id, 0,-1)
+  else
+    hl_virt_enable = true
+    M.ts_hl_groups()
+  end
+end
+
+function M.ts_hl_groups()
+  if not hl_virt_enable then return end
+
+  local ns = api.nvim_get_namespaces().treesitter_hl
+  local _, lnum, lcol = unpack(vim.fn.getcurpos())
+  local extmarks = api.nvim_buf_get_extmarks(0, ns, {lnum-1, lcol-1}, {lnum-1, -1}, { details = true })
+  local groups = {}
+
+  for i, ext in ipairs(extmarks) do
+    local infos = ext[4]
+    if infos then
+      local str = infos.hl_group
+      if i ~= #extmarks then
+        str = str..' > '
+      end
+      table.insert(groups, {str, infos.hl_group})
+    end
+  end
+
+  api.nvim_buf_clear_namespace(0, hl_ns_id, 0,-1)
+  api.nvim_buf_set_virtual_text(0, hl_ns_id, lnum-1, groups, {})
 end
 
 return M
