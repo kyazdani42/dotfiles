792227d8f80a0f72665163512f1c14bc53d40aa6
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-10-26 17:58:51 +0100

    load fzf from etc

--- config/common/zsh/fzf.zsh
+++ config/common/zsh/fzf.zsh
@@ -5,8 +5,7 @@ export FZF_ALT_C_COMMAND="fd --type d -HLi . . 2>/dev/null"
 export FZF_DEFAULT_COMMAND="rg --hidden -l "" -g '!.git' . 2>/dev/null"
 export FZF_PREVIEW_COMMAND="bat --decorations=never --theme=ansi-dark --color always {} 2>/dev/null"
 
-source /usr/share/fzf/key-bindings.zsh
-source /usr/share/fzf/completion.zsh
+source /etc/profile.d/fzf.zsh
 
 _fzf_compgen_path() {
     fd -HLi . "$1" 2>/dev/null
