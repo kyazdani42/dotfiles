16d716e7d3dcd292dae47d1f479dfc79347a1249
kiyan42 <yazdani.kiyan@protonmail.com>
2020-12-27 19:15:26 +0100

    update configs for my tree

--- config/common/nvim/lua/tree-config.lua
+++ config/common/nvim/lua/tree-config.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 function M.setup()
-  vim.g.lua_tree_ignore = { '.git', 'node_modules', 'dist' }
-  vim.g.lua_tree_auto_open = 1
-  vim.g.lua_tree_auto_close = 0
-  vim.g.lua_tree_follow = 1
-  vim.g.lua_tree_tab_open = 1
-  vim.g.lua_tree_show_icons = {
+  vim.g.nvim_tree_ignore = { '.git', 'node_modules', 'dist' }
+  vim.g.nvim_tree_auto_open = 1
+  vim.g.nvim_tree_auto_close = 0
+  vim.g.nvim_tree_follow = 1
+  vim.g.nvim_tree_tab_open = 1
+  vim.g.nvim_tree_show_icons = {
     git = 1,
     folders = 1,
     files = 1
   }
 
-  vim.g.lua_tree_icons = {
+  vim.g.nvim_tree_icons = {
     default = '',
     git= {
       unstaged = "✗",
@@ -27,15 +27,15 @@ function M.setup()
     }
   }
 
-  vim.g.lua_tree_bindings = {
+  vim.g.nvim_tree_bindings = {
     preview = { '<Tab>' }
   }
 
-  vim.api.nvim_set_keymap('n', '<C-n>', ':LuaTreeToggle<CR>', {
+  vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {
       noremap = true,
       silent = true
     })
-  vim.api.nvim_set_keymap('n', '<leader>r', ':LuaTreeRefresh<CR>', {
+  vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', {
       noremap = true,
       silent = true
     })
