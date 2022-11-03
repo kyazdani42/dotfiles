825b5a53eb316bd3887e2859be9e7617cc5b7670
yazdani kiyan <yazdani.kiyan@proton.me>
2022-11-03 14:02:08 +0100

    fix: put dconf binary in config

--- install-scripts/dconf.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-dconf write /org/gnome/desktop/interface/cursor-size 16
-dconf write /org/gnome/desktop/interface/cursor-theme "volantes_cursors"
-dconf write /org/gnome/desktop/interface/font-name "DejaVu Sans 12"
-dconf write /org/gnome/desktop/interface/gtk-theme "palenight"
-dconf write /org/gnome/desktop/interface/icon-theme "la-capitaine"
