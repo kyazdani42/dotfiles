b89e38f0c69246c2f402865c118385fc752d7d06
yazdani kiyan <yazdani.kiyan@proton.me>
2022-11-05 19:09:22 +0100

    feat: battery check as systemd service
    also remove custom wl-paste since it was an old workaround that should
    be fixed now.

--- install-scripts/systemd.sh
+++ install-scripts/systemd.sh
@@ -4,6 +4,10 @@ function sys() {
   systemctl --user $@
 }
 
-sys daemon-reload
 sys start rss.service
 sys enable rss.service
+
+sed "s/{{USER}}/$(whoami)/g" ./config/systemd/user/battery.service.in > config/systemd/user/battery.service
+
+sys start battery.service
+sys enable battery.service
