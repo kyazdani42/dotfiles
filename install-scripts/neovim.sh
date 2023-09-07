89ecf8e331a847e58afaa36fe6e872d0f8438b3b
yazdani kiyan <yazdani.kiyan@proton.me>
2023-09-07 17:47:59 +0200

    feat(firefox): adapt pixel size config depending on screen size
    - When firefox launches from sway binding, it first runs a script that
    determines if the current focused screen is wider than 1920 pixels
    width, and sets 2 to the size pixel if so, modifying the user.js firefox
    config file.
    - remove -o flag for clipboard since it doesn't work
    - update neovim install script to only install blue moon as plugin

--- install-scripts/neovim.sh
+++ install-scripts/neovim.sh
@@ -2,23 +2,13 @@
 
 set -eo pipefail
 
-BASE_DIR="$HOME/dev/nvim"
-NVIM_CONFIG_DIR="${BASE_DIR}/config" 
-NVIM_PLUGIN_DIR="${BASE_DIR}/plugins" 
-
-mkdir -p "$NVIM_PLUGIN_DIR"
+NVIM_STORAGE_DIR="$HOME/dev/nvim"
+NVIM_CONFIG_DIR="${NVIM_STORAGE_DIR}/config" 
 
 echo "Start neovim configuration"
 echo "Clone local repositories"
 
-nvim_plugs=("blue-moon" "nvim-tree.lua" "nvim-treesitter" "nvim-web-devicons")
-for repo_name in ${nvim_plugs[@]}; do
-    REPO_DIR="${NVIM_PLUGIN_DIR}/${repo_name}"
-    [ ! -d "$REPO_DIR" ] && {
-      echo "Installing ${repo_name}"
-      git clone "git@github.com:kyazdani42/${repo_name}" "$REPO_DIR" &>/dev/null
-    }
-done
+git clone "git@github.com:kyazdani42/blue-moon" "${NVIM_STORAGE_DIR}/blue-moon"
 
 if [ ! -d "$NVIM_CONFIG_DIR" ]; then
     echo "Installing neovim configurations..."
